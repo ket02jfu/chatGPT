ee632e336fc7685f2ab6c686005d7af391dc3f13
Artyom <vukolovartem228@gmail.com>
2023-03-19 18:43:33 +0100

    update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://vam-chatgpt.onrender.com', {
+    const response = await fetch('https://vamgpt.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on https://vam-chatgpt.onrender.com'))
+app.listen(5000, () => console.log('AI server started on https://vamgpt.onrender.com'))
